5b03533ae94595658edeb279c8ff996e2d304877
Berkeley19 <31030980+Berkeley19@users.noreply.github.com>
2019-05-11 15:20:08 +0100

    Frontend so far

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+
+    {
+        'author' : 'joe',
+        'title' : 'posts',
+        'content' : 'succ your mom',
+        'date_posted' : 'June 17, 2019'
+    },
+    {
+        'author': 'berk',
+        'title': 'posts2',
+        'content': 'succ your mom part 2',
+        'date_posted': 'June 18, 2019'
+    }
+]
 
 def login(request):
-    return HttpResponse('<h1>Login</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Authentication/login.html', context)
 
 def register(request):
-    return HttpResponse('<h1>Register</h1>')
+    return render(request, 'Authentication/register.html', {'title': 'About'})
+
+
