dd09e3df14cb572bf1bdd96e63f1598b60e81a34
JosephTTD <josephdada15@gmail.com>
2019-05-18 01:59:12 +0100

    more incredible front end augmentations

--- Users/urls.py
+++ Users/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .forms import UserLoginForm
 
 urlpatterns = [
-    path('', views.login_view, name='Users-login',),
+    path('', views.home_view, name='Users-home'),
+    path('login/', views.login_view, name='Users-login',),
     path('register/', views.register_view, name='Users-register',),
 ]
 
