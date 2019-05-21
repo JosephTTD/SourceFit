115eff8f079f793d27f4ed86e6461128cd1835d2
JosephTTD <josephdada15@gmail.com>
2019-05-21 21:52:59 +0100

    updates

--- Users/urls.py
+++ Users/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path('', views.home_view, name='Users-home'),
     path('login/', views.login_view, name='Users-login',),
     path('register/', views.register_view, name='Users-register',),
+    path('dashboard/', views.dashboard, name='Users-dashboard'),
+    path('diet/', views.diet, name='Users-diet'),
+    path('exercise/', views.exercise, name='Users-exercise'),
+    path('profile/', views.profile, name='Users-profile'),
+    path('settings/', views.settings, name='Users-settings'),
+    path('goals/', views.goals, name='Users-goals'),
 ]
 
 
--- Users/views.py
+++ Users/views.py
@@ -37,3 +37,55 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('/Users/login.html')
+
+posts = [
+
+    {
+        'author' : 'Joe',
+        'title' : 'posts',
+        'content' : 'succ your dad instead',
+        'date_posted' : 'June 17, 2019',
+        'current_weight' : '80',
+        'goal_weight' : '67',
+        'calories_consumed' : '1270',
+        'reach_goal' : '81',
+        'First_name' : 'Joseph',
+        'Second_name' : 'Dada'
+    }
+]
+def dashboard(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Users/dashboard.html', context, {})
+
+def profile(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Users/profile.html', context, {'title': 'Profile'})
+
+def settings(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'Users/settings.html', context, {'title': 'Settings'})
+
+def diet(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Users/diet.html', context, {'title': 'Diet'})
+
+def exercise(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Users/exercise.html', context, {'title': 'Exercise'})
+
+def goals(request):
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'Users/goals.html', context, {'title': 'Goals'})
