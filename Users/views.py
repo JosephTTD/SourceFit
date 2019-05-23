f2e42ad69d1bb6e7416ae43276e38f7a81826d03
Berkeley19 <31030980+Berkeley19@users.noreply.github.com>
2019-05-23 15:05:29 +0100

    commit

--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,9 @@
+from django.template.loader import render_to_string
+
 from .forms import UserRegisterForm, UserLoginForm, GoalCreationForm, DietCreationForm, ActivityCreationForm
 from django.contrib import messages
 import datetime
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from .models import Goal, ExerciseIntensity, DietData, CustomUser, Activity, WeightMeasurementUnits
@@ -17,7 +19,6 @@ def home_view(request):
 @login_required(login_url='Users-login')
 def dashboard_view(request):
     instance = User.objects.get(username=request.user.username)
-    print(instance.weight)
     maintenance_calories = instance.calculate_maintenance_calories()
     current_weight = instance.weight
     user_weight_units = instance.weightUnits
@@ -108,16 +109,18 @@ def display_exercise_view(request):
 def display_diet_view(request):
     instance = User.objects.get(username=request.user.username)
     date_from = datetime.datetime.now() - datetime.timedelta(days=1)
-    queryset = DietData.objects.filter(user__username=instance.username, dateAdded__gte=date_from)
+    queryset = DietData.objects.filter(user__username=instance.username).values("")
+    queryAllDailyCalories = DietData.objects.filter(user__username=instance.username,
+                                                    dateAdded__gte=date_from).values_list('calorificCount',flat=True)
+    dailyCalories = 0
+    for i in queryAllDailyCalories:
+        dailyCalories += i
     context = {
-        "posts": queryset
+        'posts': queryset,
+        'dailyCalories': dailyCalories
     }
 
-    testdata = {
-        'posts':posts
-    }
-
-    return render(request, "Users/diet.html", testdata, context)
+    return render(request, "Users/diet.html", context)
 
 
 @login_required(login_url='Users-login')
@@ -126,13 +129,16 @@ def create_goal_view(request):
     if request.method == 'POST':
         form = GoalCreationForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.user = instance
-            post.save()
-            redirect('Users-goals')
+            if Goal.objects.filter(user__username=instance.username).exists():
+                return redirect('Users-goals')
+            else:
+                post = form.save(commit=False)
+                post.user = instance
+                post.save()
+                return redirect('Users-goals')
     else:
         form = GoalCreationForm()
-    return render(request, 'Users/goal_entry.html', {'form': form})
+    return render(request, 'Users/goal_entry.html', {'form': form},)
 
 
 @login_required(login_url='Users-login')
@@ -172,7 +178,7 @@ def register_view(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}.')
-            return redirect('/Users/login.html')
+            return redirect('Users-login')
     else:
         form = UserRegisterForm()
     return render(request, 'Users/register.html', {'form': form})
@@ -194,7 +200,7 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
-        return redirect('/Users/login.html')
+        return redirect('Users-login')
 
 
 posts = [
